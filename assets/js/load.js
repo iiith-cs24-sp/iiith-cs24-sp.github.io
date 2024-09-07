8d1e97744d93c8cb44f5a1bbd562aea8fdb969c4
S P <swastik.pal@students.iiit.ac.in>
2024-09-07 18:31:12 +0530

    Load page with button

--- assets/js/load.js
+++ assets/js/load.js
@@ -1,4 +1,14 @@
 function loadPage() {
 	let page = document.getElementById("main_content");
 	page.innerHTML = "Loading...";
+	// Go to content page
+	fetch("./content/personal_info.html")
+		.then(response => response.text())
+		.then(data => {
+			// The data is another HTML page
+			// set the innerHTML of the body to the body of the data
+			let newPage = new DOMParser().parseFromString(data, "text/html");
+			console.log(newPage);
+			document.body.innerHTML = newPage.body.innerHTML;
+		});
 }
